78fb8d6931f56606ba0d7e2f0acab31c11c32fef
Matthew Loh <mattloh111@gmail.com>
2016-02-11 23:33:51 -0800

    Initialized repository with .gitignore

---
+++ FriendrTests/FriendrTests.swift
@@ -0,0 +1,36 @@
+//
+//  FriendrTests.swift
+//  FriendrTests
+//
+//  Created by Sau Chung Loh on 2/11/16.
+//  Copyright © 2016 FriendrTeam. All rights reserved.
+//
+
+import XCTest
+@testable import Friendr
+
+class FriendrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
